e6a13dde60f40a3d43ca4faa4017208b24319a60
Muhammad Ramdhani <mramdhani010@gmail.com>
2022-04-07 22:29:03 +0700

    migration first feature

---
+++ database/migrations/2022_04_07_151344_create_post_image_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_image', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('post_id');
+            $table->string('url_image');
+            $table->timestamps();
+
+            $table->foreign('post_id')->references('id')->on('post')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_image');
+    }
+};
