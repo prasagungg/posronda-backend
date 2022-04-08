8c9abe6a232aadd4f578c21e8aa218e577d6f92d
Muhammad Ramdhani <mramdhani010@gmail.com>
2022-04-08 13:34:04 +0700

    post model

---
+++ app/Models/Post.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'post';
+    protected $guarded = [];
+
+    protected function serializeDate($date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function images()
+    {
+        return $this->hasMany(PostImage::class, 'post_id');
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(PostLike::class, 'post_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(PostComment::class, 'post_id');
+    }
+}
---
+++ app/Models/PostComment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostComment extends Model
+{
+    use HasFactory;
+
+    protected $table = 'post_comment';
+    protected $guarded = [];
+
+    protected function serializeDate($date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class, 'post_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
