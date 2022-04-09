6cc20c2abd31251eb68fea45776d184c7f133e6a
Muhammad Ramdhani <mramdhani010@gmail.com>
2022-04-09 18:56:13 +0700

    comment

--- app/Http/Services/Comment/CommentQueries.php
+++ app/Http/Services/Comment/CommentQueries.php
@@ -7,10 +7,16 @@ use App\Models\PostComment;
 
 class CommentQueries extends Service
 {
-    public function getCommentByPost($post_id, $limit = 10, $orderby = null, $sort = 'asc', $filter = [])
+    public function getCommentByPost($post_id, $limit = 10, $orderby = 'created_at', $sort = 'desc', $filter = [])
     {
         $comments = PostComment::where('post_id', $post_id)->where('parent_id', null)
-            ->with(['user:id,username,is_verified,profile_picture', 'replies']);
+            ->with([
+                'user:id,name,username,is_verified,profile_picture',
+                'replies' => function ($query) {
+                    $query->orderBy('created_at', 'ASC');
+                },
+                'replies.user:id,name,username,is_verified,profile_picture'
+            ]);
 
         $sorted = $this->sorting($comments, $orderby, $sort, (new PostComment())->getTable());
         $filtered = $this->filter($sorted, $filter);
