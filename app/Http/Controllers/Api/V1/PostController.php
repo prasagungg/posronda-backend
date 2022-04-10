4e48cb7a95f3d89cf332deedc0daae8a532e1725
Muhammad Ramdhani <mramdhani010@gmail.com>
2022-04-11 05:37:20 +0700

    update function name error response

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -39,7 +39,7 @@ class PostController extends Controller
                     }
                 }
 
-                return response()->errorValidation($errors);
+                return response()->error($errors);
             }
 
             $post = $this->postCommands->create($request);
