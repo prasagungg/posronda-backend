9212a570db757b5302912876fbdf52308b325710
Muhammad Ramdhani <mramdhani010@gmail.com>
2022-04-21 21:08:16 +0700

    add cors origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://posronda.xyz'],
+    'allowed_origins' => ['https://posronda.xyz', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
