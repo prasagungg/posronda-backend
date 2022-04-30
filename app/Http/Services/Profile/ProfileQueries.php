2f42a35fd31e4d7811e0b651140b81581311a350
Muhammad Ramdhani <mramdhani010@gmail.com>
2022-04-30 09:49:33 +0700

    check followed_by_me

--- app/Http/Services/Profile/ProfileQueries.php
+++ app/Http/Services/Profile/ProfileQueries.php
@@ -5,13 +5,20 @@ namespace App\Http\Services\Profile;
 use App\Http\Services\Service;
 use App\Models\User;
 use Exception;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileQueries extends Service
 {
     public function getProfileByUsername($username)
     {
-        $user = User::username($username)->withCount(['posts'])->withCount(['followers', 'following'])->first();
+        $user = User::username($username)->withCount(['posts', 'followers', 'following'])
+            ->when(Auth::check(), function ($query) {
+                $query->withCount(['followers as followed_by_me' => fn ($follow) => $follow->wherehas('user', fn ($user) => $user->where('id', self::$user->id))]);
+            })
+            ->first();
         if (empty($user)) throw new Exception("User not found", 404);
+
+        $user->followed_by_me = $user->followed_by_me > 0;
 
         return $user;
     }
